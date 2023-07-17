02a8838f8c837cbc7716090bb1aa9b9d84e228a2
Jeong seulho <wjdtmfgh@gmail.com>
2023-07-17 17:31:39 +0900

    Design: add Slide Effect

--- FE/src/components/tab/RoleContent.tsx
+++ FE/src/components/tab/RoleContent.tsx
@@ -7,6 +7,8 @@ import { useState, useEffect, useRef } from "react";
 const RoleContent = () => {
   const [curViewRoleItemsIndex, setCurViewRoleItemsIndex] = useState(0);
   const slideRef = useRef<HTMLDivElement>(null);
+  const minViewRoleItemsIndex = 0;
+  const maxViewRoleItemsIndex = 1;
   const slideDown = () => {
     setCurViewRoleItemsIndex((prevSlide) => prevSlide + 1);
   };
@@ -16,6 +18,7 @@ const RoleContent = () => {
   useEffect(() => {
     if (slideRef.current) slideRef.current.style.transform = `translateY(-${curViewRoleItemsIndex * 80}%)`;
   }, [curViewRoleItemsIndex]);
+
   const renderViewRoleItems = (index: number) => {
     return allRoleItemsInfo[index].map((roleItemInfo) => (
       <RoleItem
@@ -28,22 +31,27 @@ const RoleContent = () => {
   };
 
   return (
-    <div className=" overflow-hidden max-h-full">
-      <img
-        src={upArrow}
-        alt=""
-        className=" cursor-pointer w-[66px] h-[36px] mx-auto hover:scale-150 transition-all duration-500"
-        onClick={slideUp}
-      />
-      <div className="flex flex-col gap-2 transition-transform duration-500" ref={slideRef}>
-        {renderViewRoleItems(curViewRoleItemsIndex)}
+    <div className=" relative w-full h-full overflow-hidden">
+      {curViewRoleItemsIndex === minViewRoleItemsIndex && (
+        <img
+          src={downArrow}
+          alt=""
+          className=" z-10 absolute bottom-4 right-4 cursor-pointer w-[56px] h-[36px] mx-auto mt-6 hover:scale-110 border-2 rounded-lg transition-all duration-500 p-2"
+          onClick={slideDown}
+        />
+      )}
+      {curViewRoleItemsIndex === maxViewRoleItemsIndex && (
+        <img
+          src={upArrow}
+          alt=""
+          className=" z-10 absolute -top-4 right-4 cursor-pointer w-[56px] h-[36px] mx-auto mt-6 hover:scale-110 border-2 rounded-lg transition-all duration-500 p-2"
+          onClick={slideUp}
+        />
+      )}
+      <div ref={slideRef} className=" duration-500 transition-all">
+        <div className=" flex flex-col gap-4">{renderViewRoleItems(0)}</div>
+        <div className=" flex flex-col gap-4  mt-[800px]">{renderViewRoleItems(1)}</div>
       </div>
-      <img
-        src={downArrow}
-        alt=""
-        className=" cursor-pointer w-[66px] h-[36px] mx-auto mt-6 hover:scale-150 transition-all duration-500"
-        onClick={slideDown}
-      />
     </div>
   );
 };
