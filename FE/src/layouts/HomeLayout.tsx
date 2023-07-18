f64e7d94c8e9bc61901ed59cd15b6136dc56cd84
Jeong seulho <wjdtmfgh@gmail.com>
2023-07-19 08:07:06 +0900

    Fix: HomeLayout background Img 렌더링 안되는 버그

--- FE/src/layouts/HomeLayout.tsx
+++ FE/src/layouts/HomeLayout.tsx
@@ -4,7 +4,7 @@ import { LayoutChildrenProps } from "../types/LayoutChildrenProps";
 const HomeLayout = ({ children }: LayoutChildrenProps) => {
   return (
     <div
-      className={`w-full h-screen bg-contain bg-black  bg-no-repeat  bg-center relative`}
+      className="w-full h-screen bg-contain bg-black  bg-no-repeat  bg-center relative"
       style={{ backgroundImage: `url(${mainImg})` }}
     >
       {children}
