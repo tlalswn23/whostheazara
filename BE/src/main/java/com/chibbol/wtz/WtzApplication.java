0844a2cbbf24e725278160ebbe0b33213b2ba4ad
tlalswn23 <tlalswn23@daum.net>
2023-07-18 15:43:57 +0900

    BE 폴터구조 수정

--- BE/src/main/java/com/chibbol/wtz/WtzApplication.java
+++ BE/src/main/java/com/chibbol/wtz/WtzApplication.java
@@ -1,7 +1,9 @@
 package com.chibbol.wtz;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class WtzApplication {
