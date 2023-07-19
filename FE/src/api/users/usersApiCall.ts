30d7bac5491efc37b9060420792ab5f5a8309974
Jeong seulho <wjdtmfgh@gmail.com>
2023-07-19 11:01:55 +0900

    Refactor: return null 위치 변경

--- FE/src/api/users/usersApiCall.ts
+++ FE/src/api/users/usersApiCall.ts
@@ -12,26 +12,20 @@ export const sendEmailVerificationCodeWithSignup = async (email: string) => {
     toast.success("인증코드가 발송되었습니다.");
     return true;
   } catch (error: unknown) {
-    if (!(error instanceof CustomErrorClass)) {
-      return null;
-    }
-    if (!error.response?.status) {
-      return null;
-    }
-
-    const { status } = error.response;
-    switch (status) {
-      case 429:
-        toast.error("인증코드 발송 횟수를 초과하였습니다.");
-        break;
-      case 502:
-        toast.error("이미 가입된 이메일입니다.");
-        break;
+    if (error instanceof CustomErrorClass) {
+      const { status } = error.response!;
+      switch (status) {
+        case 429:
+          toast.error("인증코드 발송 횟수를 초과하였습니다.");
+          break;
+        case 502:
+          toast.error("이미 가입된 이메일입니다.");
+          break;
+      }
     }
     return null;
   }
 };
-
 export const login = async (email: string, password: string) => {
   const url = usersUrl.login();
   const payload = { email, password };
@@ -43,25 +37,20 @@ export const login = async (email: string, password: string) => {
     toast.success("로그인 되었습니다.");
     return true;
   } catch (error: unknown) {
-    if (!(error instanceof CustomErrorClass)) {
-      return null;
+    if (error instanceof CustomErrorClass) {
+      const { status } = error.response!;
+      switch (status) {
+        case 401:
+          toast.error("비밀번호가 일치하지 않습니다.");
+          break;
+        case 404:
+          toast.error("가입되지 않은 이메일입니다.");
+          break;
+      }
     }
-    if (!error.response?.status) {
-      return null;
-    }
-
-    const { status } = error.response;
-    switch (status) {
-      case 401:
-        toast.error("비밀번호가 일치하지 않습니다.");
-        break;
-      case 404:
-        toast.error("가입되지 않은 이메일입니다.");
-        break;
-    }
+    return null;
   }
 };
-
 export const signup = async (email: string, password: string, nickname: string, verificationCode: string) => {
   const url = usersUrl.signUp();
   const payload = { email, password, nickname, verificationCode };
@@ -70,24 +59,19 @@ export const signup = async (email: string, password: string, nickname: string, 
     toast.success("회원가입이 완료되었습니다.");
     return true;
   } catch (error: unknown) {
-    if (!(error instanceof CustomErrorClass)) {
-      return null;
-    }
-    if (!error.response?.status) {
-      return null;
-    }
-
-    const { status } = error.response;
-    switch (status) {
-      case 400:
-        toast.error("인증코드가 일치하지 않습니다.");
-        break;
-      case 409:
-        toast.error("이미 가입된 이메일입니다.");
-        break;
-      case 422:
-        toast.error("입력 형식이 올바르지 않습니다.");
-        break;
+    if (error instanceof CustomErrorClass) {
+      const { status } = error.response!;
+      switch (status) {
+        case 400:
+          toast.error("인증코드가 일치하지 않습니다.");
+          break;
+        case 409:
+          toast.error("이미 가입된 이메일입니다.");
+          break;
+        case 422:
+          toast.error("입력 형식이 올바르지 않습니다.");
+          break;
+      }
     }
     return null;
   }
@@ -100,11 +84,31 @@ export const logout = async () => {
     toast.success("로그아웃 되었습니다.");
     return true;
   } catch (error: unknown) {
-    if (!(error instanceof CustomErrorClass)) {
+    if (error instanceof CustomErrorClass) {
       return null;
     }
-    if (!error.response?.status) {
-      return null;
+  }
+};
+
+export const sendEmailVerificationCodeWithResetPw = async (email: string) => {
+  const url = usersUrl.sendEmailVerificationCodeWhenSignup();
+  const payload = { email };
+  try {
+    await axios.post(url, payload);
+    toast.success("인증코드가 발송되었습니다.");
+    return true;
+  } catch (error: unknown) {
+    if (error instanceof CustomErrorClass) {
+      const { status } = error.response!;
+      switch (status) {
+        case 429:
+          toast.error("인증코드 발송 횟수를 초과하였습니다.");
+          break;
+        case 502:
+          toast.error("이미 가입된 이메일입니다.");
+          break;
+      }
     }
+    return null;
   }
 };
