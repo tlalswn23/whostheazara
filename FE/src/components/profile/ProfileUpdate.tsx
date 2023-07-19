a0241d0127248282ada36dab1ba6ebadb88c5be4
J3SUNG <wptjd6141@naver.com>
2023-07-19 17:02:25 +0900

    Feat: 회원정보수정, 게임전적조회, 게임전적통계 개발

--- FE/src/components/profile/ProfileUpdate.tsx
+++ FE/src/components/profile/ProfileUpdate.tsx
@@ -1,29 +1,15 @@
 import { useState } from "react";
 import yellowBtnImg from "../../assets/img/yellowBtnImg.png";
+import { ProfileInputForm } from "./ProfileInputForm";
 
 export const ProfileUpdate = () => {
-  const [selectedNum, setSelectedNum] = useState(1);
-  const [selectedJob, setSelectedJob] = useState(0);
-
-  const onSetSelectedNum = (num: number) => {
-    setSelectedNum(num);
-  };
-
-  const onSetSelectedJob = (num: number) => {
-    if ((selectedJob & (1 << num)) != 0) {
-      setSelectedJob(selectedJob & ~(1 << num));
-    } else {
-      setSelectedJob(selectedJob | (1 << num));
-    }
-  };
-
-  const num = [...Array(4).keys()];
-  const job = [...Array(8).keys()];
-
   return (
     <>
-      <div className="absolute left-[36%] top-[17%] w-[1140px] h-[700px] border-solid border-white border-[20px] p-[80px] text-[56px] font-bold bg-black">
-        <div className="absolute w-[360px] h-[120px] flex justify-center items-center bottom-[-50px] right-[-60px]">
+      <div className="absolute left-[690px] top-[160px] w-[1140px] h-[700px] border-solid border-white border-[20px] p-[80px] text-[48px] font-bold bg-black flex flex-col justify-around">
+        <ProfileInputForm text="닉네임" />
+        <ProfileInputForm text="새 비밀번호" />
+        <ProfileInputForm text="비밀번호 확인" />
+        <div className="absolute w-[360px] h-[120px] flex justify-center items-center bottom-[-50px] right-[-60px] text-[56px]">
           <img src={yellowBtnImg} className="absolute" />
           <p className="absolute">수정</p>
         </div>
