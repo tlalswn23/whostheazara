7dd7fee86634cb10837809b32f569046f291e56a
J3SUNG <wptjd6141@naver.com>
2023-07-21 17:52:46 +0900

    Feat: 반응형 웹 브런치 생성

--- FE/src/components/home/HomeBtn.tsx
+++ FE/src/components/home/HomeBtn.tsx
@@ -7,6 +7,7 @@ interface HomeBtnProps {
 }
 
 export const HomeBtn = ({ text, index, onClick }: HomeBtnProps) => {
+  // 텍스트 오류 발생
   const [indexStyle, setIndexStyle] = useState([
     "text-[9vh] top-[16%] left-[3%] rotate-[4deg] text-yellow-200",
     "text-[4vh] top-[48%] left-[-1%] rotate-[8deg] text-white",
