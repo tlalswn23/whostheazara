4f54681b93c6ddaff1a99f0d29b11e63358002fb
J3SUNG <wptjd6141@naver.com>
2023-07-21 10:11:19 +0900

    Feat: 로비 직업 선택 수정

--- FE/src/components/Lobby/LobbyJobBtn.tsx
+++ FE/src/components/Lobby/LobbyJobBtn.tsx
@@ -1,4 +1,8 @@
-import test from "../../assets/img/rabbitImg.png";
+import jobPolice from "../../assets/img/jobPolice.png";
+import jobDoctor from "../../assets/img/jobDoctor.png";
+import jobArmy from "../../assets/img/jobArmy.png";
+import jobPolitician from "../../assets/img/jobPolitician.png";
+import jobThug from "../../assets/img/jobThug.png";
 
 interface LobbyJobBtn {
   index: number;
@@ -11,12 +15,14 @@ export const LobbyJobBtn = ({ index, selectedJob, onSetSelectedJob }: LobbyJobBt
     onSetSelectedJob(index);
   };
 
+  const job = [jobPolice, jobDoctor, jobArmy, jobPolitician, jobThug];
+
   return (
     <>
       <div className="relative w-[120px] h-[120px] mx-[36px] my-[40px] cursor-pointer" onClick={() => onSetToggleJob()}>
-        <img src={test} className="w-full h-full" />
+        <img src={job[index]} className="w-full h-full" />
         {(selectedJob & (1 << index)) != 0 ? (
-          <p className="absolute top-[-30px] left-[24px] text-[100px] text-red-600">X</p>
+          <p className="absolute top-[-42px] left-[21px] text-[128px] text-red-600">X</p>
         ) : (
           ""
         )}
