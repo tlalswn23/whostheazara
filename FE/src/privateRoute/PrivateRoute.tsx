111b37bd7bae31d1f860d898e2c893e9e97af68c
Jeong seulho <wjdtmfgh@gmail.com>
2023-07-24 10:54:18 +0900

    Feat: framer motion lib 설치

--- FE/src/privateRoute/PrivateRoute.tsx
+++ FE/src/privateRoute/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
-import { useAccessTokenState } from "../../context/loginContext";
-import ForbiddenAuth from "../../pages/ForbiddenAuth";
+import { useAccessTokenState } from "../context/loginContext";
+import ForbiddenAuth from "../pages/ForbiddenAuth";
 
 interface PrivateRouteProps {
   children?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Componet Element
@@ -9,7 +9,9 @@ interface PrivateRouteProps {
 }
 
 export function PrivateRoute({ requireAuth }: PrivateRouteProps): React.ReactElement | null {
-  const { accessToken } = useAccessTokenState();
+  // const { accessToken } = useAccessTokenState();
+  //TODO: 수정
+  const accessToken = true;
 
   if (requireAuth) {
     // 인증이 반드시 필요한 페이지인 경우
