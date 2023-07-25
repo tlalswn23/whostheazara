5e31dd4379e197d50f3b13c0e58578b1308a161f
Jeong seulho <wjdtmfgh@gmail.com>
2023-07-25 13:04:36 +0900

    Chore: fix room chat

--- FE/src/pages/Room.tsx
+++ FE/src/pages/Room.tsx
@@ -3,10 +3,6 @@ import { RoomExitBtn } from "../components/room/RoomExitBtn";
 import { RoomChat } from "../components/room/RoomChat";
 import { RoomUserList } from "../components/room/RoomUserList";
 import { RoomLayout } from "../layouts/RoomLayout";
-import { useWebSocket } from "../context/socketContext";
-import { useEffect } from "react";
-import { useState } from "react";
-import { StompChatType } from "../types/StompChatType";
 
 export const Room = () => {
   return (
