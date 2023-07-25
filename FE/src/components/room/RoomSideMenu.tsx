f68f590245f374510b5ab0b7004faef859a44b4a
J3SUNG <wptjd6141@naver.com>
2023-07-25 13:19:44 +0900

    Feat: 1536*754 (Room)

--- FE/src/components/room/RoomSideMenu.tsx
+++ FE/src/components/room/RoomSideMenu.tsx
@@ -106,7 +106,7 @@ export const RoomSideMenu = () => {
   return (
     <aside className="relative 3xl:mb-[30px] mb-[24px] 3xl:w-[550px] w-[440px] 3xl:h-[720px] h-[576px] text-white">
       <img src={roomChat} className="absolute left-[0px] top-[0px] w-[full]" />
-      <div className="absolute top-[60px] left-[40px] text-[28px] pr-[10px] overflow-y-scroll 3xl:h-[540px] h-[432px] 3xl:w-[490px] w-[392px]">
+      <div className="absolute 3xl:top-[60px] top-[48px] 3xl:left-[40px] left-[36px] 3xl:text-[28px] text-[22.4px] 3xl:pr-[10px] pr-[8px] overflow-y-scroll 3xl:h-[540px] h-[432px] 3xl:w-[490px] w-[392px]">
         {chatData.map((item, index) => (
           <p className={`${itemColor[item.userOrder]}`} key={index}>
             {item.nickname} : {item.content}
@@ -114,7 +114,7 @@ export const RoomSideMenu = () => {
         ))}
       </div>
       <input
-        className="absolute 3xl:w-[510px] w-[408px] 3xl:h-[60px] h-[48px] left-[20px] bottom-[20px] text-black 3xl:px-[20px] px-[16px] 3xl:text-[28px] text-[22.4px]"
+        className="absolute 3xl:w-[510px] w-[408px] 3xl:h-[60px] h-[48px] 3xl:left-[20px] left-[16px] 3xl:bottom-[20px] bottom-[16px] text-black 3xl:px-[20px] px-[16px] 3xl:text-[28px] text-[22.4px]"
         value={inputChat}
         onChange={onChange}
       />
