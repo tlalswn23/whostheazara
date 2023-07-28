f0e6366b653fe060825b69b3cccadc2e98540a6e
J3SUNG <wptjd6141@naver.com>
2023-07-28 16:09:24 +0900

    Fix: 결과화면 오류 수정

--- FE/src/components/result/ResultForm.tsx
+++ FE/src/components/result/ResultForm.tsx
@@ -8,18 +8,18 @@ export const ResultForm = () => {
     <>
       <div
         className={`flex flex-col justify-around w-full h-full bg-gradient-to-b from-black from-20% font-bold ${
-          rabbitWin !== 2 ? "to-yellow-200" : "to-green-200"
+          rabbitWin < 2 ? "to-yellow-200" : "to-green-200"
         }`}
       >
-        <div className={`flex justify-center ${rabbitWin !== 2 ? "text-red-500" : "text-white"}`}>
+        <div className={`flex justify-center ${rabbitWin < 2 ? "text-red-500" : "text-white"}`}>
           {user.map((item, index) => {
             return item !== rabbitWin && <ResultLose index={index} key={index} />;
           })}
         </div>
-        <p className={`text-[72px] text-center font-bold ${rabbitWin !== 2 ? "text-yellow-200" : "text-green-200"}`}>
-          {rabbitWin !== 2 ? "토끼 승리" : "자라 승리"}
+        <p className={`text-[72px] text-center font-bold ${rabbitWin < 2 ? "text-yellow-200" : "text-green-200"}`}>
+          {rabbitWin < 2 ? "토끼 승리" : "자라 승리"}
         </p>
-        <div className={`flex justify-center ${rabbitWin === 2 ? "text-red-500" : "text-white"}`}>
+        <div className={`flex justify-center ${rabbitWin > 2 ? "text-red-500" : "text-white"}`}>
           {user.map((item, index) => {
             return item === rabbitWin && <ResultWin index={index} key={index} />;
           })}
