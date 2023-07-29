83e323109763818d97d69a26f8c2b4daf5753fd6
Jeong seulho <wjdtmfgh@gmail.com>
2023-07-29 23:57:36 +0900

    Fix: refreshToken으로 로그인 여부 판별

--- FE/src/privateRoute/PrivateRoute.tsx
+++ FE/src/privateRoute/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
-import { useAccessTokenState } from "../context/loginContext";
 import ForbiddenAuth from "../pages/ForbiddenAuth";
+import { getRefreshToken } from "../utils/cookie";
 
 interface PrivateRouteProps {
   children?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Componet Element
@@ -9,15 +9,15 @@ interface PrivateRouteProps {
 }
 
 export function PrivateRoute({ requireAuth }: PrivateRouteProps): React.ReactElement | null {
-  const { accessToken } = useAccessTokenState();
+  const { refreshToken } = getRefreshToken();
 
   //FIXME: requireAuth
-  if (false) {
+  if (requireAuth) {
     // 인증이 반드시 필요한 페이지인 경우
-    return accessToken ? <Outlet /> : <ForbiddenAuth />;
+    return refreshToken ? <Outlet /> : <ForbiddenAuth />;
   } else {
     // 인증이 반드시 없어야 하는 페이지인 경우
-    return accessToken ? <ForbiddenAuth /> : <Outlet />;
+    return refreshToken ? <ForbiddenAuth /> : <Outlet />;
   }
 }
 
