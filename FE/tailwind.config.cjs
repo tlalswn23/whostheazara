fc531b69078a820dcbcded7ff2e62bc7a751083a
wptjd6141@naver.com <wptjd6141@naver.com>
2023-07-30 11:38:59 +0900

    Feat: 애니메이션 추가, 배경 이미지 소스 변경

--- FE/tailwind.config.cjs
+++ FE/tailwind.config.cjs
@@ -20,10 +20,46 @@ export default {
             opacity: "0",
           },
         },
+        "slide-left": {
+          "0%": {
+            transform: "translateX(-100%)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+        "slide-right": {
+          "0%": {
+            right: "-100%",
+          },
+          "100%": {
+            right: "0%",
+          },
+        },
+        "slide-top": {
+          "0%": {
+            top: "-100%",
+          },
+          "100%": {
+            top: "0%",
+          },
+        },
+        "slide-bottom": {
+          "0%": {
+            bottom: "-100%",
+          },
+          "100%": {
+            bottom: "0%",
+          },
+        },
       },
       animation: {
         "fade-in": "fade-in 0.5s ease-in-out",
         "fade-out": "fade-out 0.5s ease-in-out",
+        "slide-left": "slide-left 1.5s linear",
+        "slide-right": "slide-right 0.5s linear",
+        "slide-top": "slide-top 0.5s linear",
+        "slide-bottom": "slide-bottom 0.5s linear",
       },
       dropShadow: {
         "stroke-black": ["-4px -4px 0 #000", "4px -4px 0 #000", "-4px 4px 0 #000", "4px 4px 0 #000"],
