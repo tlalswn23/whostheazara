051fb764ae145877ad6b31da7c97568b59711a83
J3SUNG <wptjd6141@naver.com>
2023-07-31 16:08:43 +0900

    Feat: 룸 헤더 UI 보완

--- FE/src/components/room/RoomHeader.tsx
+++ FE/src/components/room/RoomHeader.tsx
@@ -5,16 +5,14 @@ import RoomJobBtn from "./RoomJobBtn";
 export const RoomHeader = () => {
   return (
     <div
-      className="3xl:w-[1420px] w-[1136px] 3xl:h-[126px] h-[100.8px] 3xl:text-[38px] text-[30.4px] text-white flex items-center justify-around bg-cover"
+      className="3xl:w-[1420px] w-[1136px] 3xl:h-[126px] h-[100.8px] 3xl:text-[38px] text-[30.4px] text-white flex items-center bg-cover"
       style={{ backgroundImage: `url("${roomTitle}")` }}
     >
-      <p className="3xl:text-[30px] text-[24px] 3xl:ml-[40px] ml-[32px] overflow-hidden text-ellipsis whitespace-nowrap">
-        221. 자라 잡으러 가실분 구해요 자라 잡으러 가실분 구해요
+      <p className="3xl:text-[30px] text-[24px] 3xl:w-[1000px] w-[800px] 3xl:ml-[50px] ml-[40px] overflow-hidden text-ellipsis whitespace-nowrap">
+        221. 자라 잡으러 가실분 구해요 자라 잡으러 가실분 구해요 자라 잡으러 가실분 구해요
       </p>
       <div className="flex justify-end w-[272px] 3xl:w-[340px]">
-        {JOB_MAP.map((job) => (
-          <RoomJobBtn key={job.id} img={job.img} id={job.id} />
-        ))}
+        {JOB_MAP.map((job) => job.id > 2 && <RoomJobBtn key={job.id} img={job.imgColor} id={job.id} />)}
       </div>
     </div>
   );
