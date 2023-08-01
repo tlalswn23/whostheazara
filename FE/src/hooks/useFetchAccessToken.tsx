4d701aadfee06457300e2003d688e0485d44f219
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-01 13:38:37 +0900

    Fix: router home

--- FE/src/hooks/useFetchAccessToken.tsx
+++ FE/src/hooks/useFetchAccessToken.tsx
@@ -5,8 +5,8 @@ import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 
 export const useFetchAccessToken = () => {
-  const navigate = useNavigate();
   let accessToken = "";
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
       try {
@@ -19,7 +19,7 @@ export const useFetchAccessToken = () => {
         if (message === "Refresh Token Not Exist") return;
         if (message === "Invalid Token") {
           toast.error("다시 로그인 해주세요.");
-          navigate("/home");
+          navigate("/");
         }
       }
     })();
