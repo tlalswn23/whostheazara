8c2633caadee3dcb912f1e3901735c3c298ec21f
J3SUNG <wptjd6141@naver.com>
2023-08-01 13:14:49 +0900

    Feat: 로비, 프로필 배경 애니메이션 추가

--- FE/src/layouts/LobbyLayout.tsx
+++ FE/src/layouts/LobbyLayout.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useEffect } from "react";
 import lobbyBg from "../assets/img/lobby/lobbyBg.png";
 import { LayoutChildrenProps } from "../types/LayoutChildrenProps";
 import MotionLayout from "./MotionLayout";
@@ -22,14 +22,14 @@ const LobbyLayout = ({ children }: LayoutChildrenProps) => {
       setDir((dir) => (dir + 1) % 4);
       setMove(translate[dir]);
       console.log(dir);
-    }, 15000);
+    }, 20000);
   }, [dir]);
 
   return (
     <MotionLayout>
       <div className={`relative 3xl:w-[1920px] w-[1536px] 3xl:h-[942px] h-[754px] overflow-hidden`}>
         <div
-          className={`absolute top-[0px] left-[-960px] w-[3840px] h-[2160px] bg-repeat transition duration-[15000ms] ease-linear ${move}`}
+          className={`absolute top-[0px] left-[-960px] w-[3840px] h-[2160px] bg-repeat transition duration-[20000ms] ease-linear ${move}`}
           style={{ backgroundImage: `url("${lobbyBg}")` }}
         ></div>
         {children}
--- FE/src/pages/Room.tsx
+++ FE/src/pages/Room.tsx
@@ -7,7 +7,7 @@ import { RoomLayout } from "../layouts/RoomLayout";
 export const Room = () => {
   return (
     <RoomLayout>
-      <div className="flex flex-wrap w-full justify-center items-center 3xl:px-[40px] px-[36px]">
+      <div className="relative flex flex-wrap w-full justify-center items-center 3xl:px-[40px] px-[36px]">
         <div className="flex items-center w-full">
           <RoomHeader />
           <RoomHeaderBtn />
