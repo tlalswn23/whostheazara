74ccbd6af883e0ebf5d6099643ce3b472d88c261
J3SUNG <wptjd6141@naver.com>
2023-08-01 09:08:45 +0900

    Feat: room list UI 수정

--- FE/src/components/lobby/LobbyRoomItem.tsx
+++ FE/src/components/lobby/LobbyRoomItem.tsx
@@ -8,7 +8,7 @@ export const LobbyRoomItem = ({ index, text, num }: LobbyListItemProps) => {
   return (
     <>
       <div
-        className={`w-[50%] h-[25%] 3xl:border-[10px] border-[8px] border-solid border-black flex items-center 3xl:text-[24px] text-[20px] text-white hover:text-yellow-200 cursor-pointer shadow-inner shadow-white`}
+        className={`w-[50%] h-[25%] 3xl:border-[10px] border-[8px] border-solid border-black flex items-center 3xl:text-[24px] text-[20px] text-white hover:text-yellow-200 cursor-pointer shadow-inner shadow-white bg-gray-900`}
       >
         <div className="flex">
           <p className="3xl:w-[64px] w-[54px] 3xl:ml-[40px] ml-[32px]">{index}.</p>
--- FE/src/privateRoute/PrivateRoute.tsx
+++ FE/src/privateRoute/PrivateRoute.tsx
@@ -13,7 +13,7 @@ export function PrivateRoute({ requireAuth }: PrivateRouteProps): React.ReactEle
   const isLogin = useIsLogin();
 
   //FIXME: requireAuth
-  if (requireAuth) {
+  if (false) {
     // 인증이 반드시 필요한 페이지인 경우
     return isLogin ? <Outlet /> : <ForbiddenAuth />;
   } else {
