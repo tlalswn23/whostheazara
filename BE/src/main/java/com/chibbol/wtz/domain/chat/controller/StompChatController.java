aed3149630e72dbdd2fd07158ffe2e14393e829a
YeahLim <yeahleem2@gmail.com>
2023-08-03 16:28:40 +0900

    Feat: jwt 수정

--- BE/src/main/java/com/chibbol/wtz/domain/chat/controller/StompChatController.java
+++ BE/src/main/java/com/chibbol/wtz/domain/chat/controller/StompChatController.java
@@ -1,8 +1,9 @@
 package com.chibbol.wtz.domain.chat.controller;
 
 import com.chibbol.wtz.domain.chat.dto.ChatMessageDTO;
-import com.chibbol.wtz.global.stomp.service.RedisPublisher;
 import com.chibbol.wtz.domain.chat.service.StompChatService;
+import com.chibbol.wtz.global.security.service.TokenService;
+import com.chibbol.wtz.global.stomp.service.RedisPublisher;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,14 +17,16 @@ public class StompChatController {
 
     private final RedisPublisher redisPublisher;
     private final StompChatService stompChatService;
+    private final TokenService tokenService;
 
     // ws 프로토콜로 사용한 "/pub/chat/enter"과 매칭
-    @Operation(summary = "채팅방 입장") // todo: 토픽 구독
+    @Operation(summary = "채팅방 입장")
     @MessageMapping(value = "/chat/enter")
     public void enter(ChatMessageDTO chatMessageDTO) {
         log.info("채팅방 입장 시작");
         log.info("chatMessageDTO : "+chatMessageDTO.toString());
         chatMessageDTO.setUserName(stompChatService.findUserName(chatMessageDTO.getUserSeq()));
+//        chatMessageDTO.setUserName(user.getNickname());
         chatMessageDTO.setMessage(chatMessageDTO.getUserName() + "님이 채팅방에 입장하셨습니다.");
         stompChatService.enterChatRoom(chatMessageDTO.getCode());
         redisPublisher.publish(stompChatService.getTopic(chatMessageDTO.getCode()), chatMessageDTO); // websocket에 발행된 메세지를 redis로 발행(publish)
