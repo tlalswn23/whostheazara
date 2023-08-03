019a4d81be778db9fbf68d95b753565536c428e7
J3SUNG <wptjd6141@naver.com>
2023-08-03 16:48:32 +0900

    Feat: 상점 UI 색 수정

--- FE/src/components/shop/ShopCharacterColor.tsx
+++ FE/src/components/shop/ShopCharacterColor.tsx
@@ -13,7 +13,7 @@ export const ShopCharacterColor = ({ color, setColor }: ShopCharacterColorProps)
           return (
             <div
               className={`3xl:w-[100px] w-[80px] 3xl:h-[100px] h-[80px] 3xl:mx-[30px] mx-[24px] 3xl:mb-[30px] mb-[24px] ${item} cursor-pointer border-solid 3xl:border-[8px] border-[6.4px] rounded-md ${
-                color === index - 1 ? `${BORDER_COLOR_MAP[index]} scale-110` : "border-white"
+                color === index - 1 && `scale-125`
               }`}
               onClick={() => setColor(index - 1)}
               key={index}
