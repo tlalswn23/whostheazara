a015f36ce0ea034b3f9c5be48735a14af6d6471b
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-03 15:28:08 +0900

    Feat: 전적 통계 api call

--- FE/src/api/axios/useRecordApiCall.tsx
+++ FE/src/api/axios/useRecordApiCall.tsx
@@ -28,7 +28,53 @@ export const useRecordApiCall = () => {
     }
   };
 
+  const getTotalWinRate = async () => {
+    const url = recordUrl.totalWinRate();
+    try {
+      const res = await interceptAxiosInstance.get(url);
+      const winRate = res.data;
+      return winRate;
+    } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      const { status } = axiosError.response!;
+
+      switch (status) {
+        case ERROR_CODE_MAP.NOT_FOUND:
+          toast.error("유저 정보가 존재하지 않습니다.");
+          break;
+        default:
+          toast.error("알 수 없는 에러가 발생했습니다, 관리자에게 문의해주세요.");
+          break;
+      }
+      throw error;
+    }
+  };
+
+  const getJobWinRate = async () => {
+    const url = recordUrl.jobWinRate();
+    try {
+      const res = await interceptAxiosInstance.get(url);
+      const winRate = res.data;
+      return winRate;
+    } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      const { status } = axiosError.response!;
+
+      switch (status) {
+        case ERROR_CODE_MAP.NOT_FOUND:
+          toast.error("유저 정보가 존재하지 않습니다.");
+          break;
+        default:
+          toast.error("알 수 없는 에러가 발생했습니다, 관리자에게 문의해주세요.");
+          break;
+      }
+      throw error;
+    }
+  };
+
   return {
     getRecentlyGameDataList,
+    getTotalWinRate,
+    getJobWinRate,
   };
 };
