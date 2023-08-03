709483433ad5b94ad87dc54dfafe0f9d5d8f1535
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-03 14:11:10 +0900

    Refactor: subRoom, unsubRoom 컴포넌트 안에서 선언

--- FE/src/components/room/RoomChat.tsx
+++ FE/src/components/room/RoomChat.tsx
@@ -2,33 +2,58 @@ import roomChat from "../../assets/img/room/roomChat.png";
 import { useState } from "react";
 import { useWebSocket } from "../../context/socketContext";
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useAccessTokenState } from "../../context/accessTokenContext";
+import chatUrl from "../../api/url/chatUrl";
 
 export const RoomChat = () => {
   const { roomCode } = useParams();
+  const location = useLocation();
   const [inputChat, setInputChat] = useState("");
-  const { client, subRoom, unSubRoom, sendMsg } = useWebSocket();
+  const { client, sendMsg } = useWebSocket();
   const [chatList, setChatList] = useState<string[]>([]);
   const { userSeq } = useAccessTokenState();
 
   // TODO: Test
   const onSendMsg = () => {
-    sendMsg(roomCode!, userSeq, inputChat);
+    if (!roomCode) return;
+    sendMsg(roomCode, userSeq, inputChat);
     setInputChat("");
   };
 
-  useEffect(() => {
-    subRoom(roomCode!, userSeq, (receiveMsg) => {
+  const subRoom = () => {
+    const url = chatUrl.subscribe(roomCode!);
+    client?.subscribe(url, (receiveMsg) => {
       const data = JSON.parse(receiveMsg.body);
       setChatList((prev) => [...prev, data.message]);
     });
+  };
+
+  const sendEnterMsg = () => {
+    const url = chatUrl.publishEnterMessage();
+    const body = JSON.stringify({ code: roomCode, userSeq });
+    client?.publish({ destination: url, body });
+  };
+
+  const unSubRoom = () => {
+    if (!roomCode) return;
+    client?.unsubscribe(chatUrl.subscribe(roomCode));
+    console.log("unsubscribed");
+  };
+
+  useEffect(() => {
+    if (!roomCode) return;
+    subRoom();
+    sendEnterMsg();
 
     return () => {
+      // location.pathname을 확인하여 /game으로 시작하는 경로로 이동하는지 확인
+      if (!location.pathname.startsWith("/game")) {
+        unSubRoom();
+      }
       setChatList([]);
     };
-  }, [roomCode, subRoom, unSubRoom, client]);
-
+  }, [roomCode, client, location]);
   return (
     <aside className="relative 3xl:mb-[30px] mb-[24px] 3xl:w-[550px] w-[440px] 3xl:h-[720px] h-[576px] text-white 3xl:ml-[25px] ml-[20px]">
       <img src={roomChat} className="absolute left-[0px] top-[0px] w-[full]" />
