7156fc62207ab06eb2d71d8d748b0ae97d75a512
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-04 20:49:14 +0900

    Fix: base url

--- FE/src/api/url/baseUrl.ts
+++ FE/src/api/url/baseUrl.ts
@@ -3,9 +3,9 @@
 // export const stompBaseUrl = "ws://14.46.141.248:8080/stomp";
 
 // 민주 싸피노트북
-export const baseUrl = "http://192.168.100.93:8080/api/v1";
-export const stompBaseUrl = "ws://14.46.141.193:8080/stomp";
+// export const baseUrl = "http://192.168.100.93:8080/api/v1";
+// export const stompBaseUrl = "ws://14.46.141.193:8080/stomp";
 
 // 배포
-// export const baseUrl = "https://i9d206.p.ssafy.io/api/v1";
-// export const stompBaseUrl = "ws://i9d206.p.ssafy.io/stomp";
+export const baseUrl = "https://i9d206.p.ssafy.io/api/v1";
+export const stompBaseUrl = "ws://i9d206.p.ssafy.io/stomp";
