0a9990444ad08d03e58b8ec4e6ae3daf2c99ae26
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-06 00:29:59 +0900

    Comment: 주석 풀기

--- FE/src/pages/Room.tsx
+++ FE/src/pages/Room.tsx
@@ -5,7 +5,7 @@ import { RoomUserList } from "../components/room/RoomUserList";
 import { RoomLayout } from "../layouts/RoomLayout";
 import { useFetchAccessToken } from "../hooks/useFetchAccessToken";
 import { useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { JobSettingType, CurSeats } from "../types/RoomSettingType";
 import { SeatInfo } from "../types/RoomSettingType";
@@ -39,43 +39,39 @@ const defaultCurSeats: CurSeats = [
 
 export const Room = () => {
   useFetchAccessToken();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { roomCode } = useParams();
   const location = useLocation();
   const [title, setTitle] = useState<string>(location.state?.title || "");
-  // const [ownerUserSeq, setOwnerUserSeq] = useState(0);
+  const [ownerUserSeq, setOwnerUserSeq] = useState(0);
   const [jobSetting, setJobSetting] = useState<JobSettingType>(location.state?.jobSetting || defaultJobSetting);
   const [curSeats, setCurSeats] = useState<CurSeats>(defaultCurSeats);
   const [chatList, setChatList] = useState<string[]>([]);
   const { client } = useWebSocket();
 
-  const subRoom = () => {
-    // const url = chatUrl.subscribe(roomCode!);
-    // client?.subscribe(url, (receive) => {
-    //   const data = JSON.parse(receive.body);
-    // TODO
-    // 받은 데이터로 채팅, 룸 정보 업데이트
-    // setTitle, setOwnerUserSeq, setJobSetting, setCurSeats, setChatList
-    // });
+  const subRoom = (roomCode: string) => {
+    const url = chatUrl.subscribe(roomCode);
+    client?.subscribe(url, (receive) => {
+      const data = JSON.parse(receive.body);
+      // TODO
+      // 받은 데이터로 채팅, 룸 정보 업데이트
+      // setTitle, setOwnerUserSeq, setJobSetting, setCurSeats, setChatList
+    });
   };
 
-  const unSubRoom = () => {
+  const unSubRoom = (roomCode: string) => {
     // TODO
     // crate된 roomCode가 아니면 서버에서 거부
-    if (!roomCode) return;
-    const url = chatUrl.subscribe(roomCode!);
+    const url = chatUrl.subscribe(roomCode);
     client?.unsubscribe(url);
   };
 
   useEffect(() => {
     if (!roomCode) return;
-    subRoom();
+    subRoom(roomCode);
 
     return () => {
-      // location.pathname을 확인하여 /game으로 시작하는 경로로 이동하는지 확인
-      if (!location.pathname.startsWith("/game")) {
-        unSubRoom();
-      }
+      unSubRoom(roomCode);
       setChatList([]);
     };
   }, [roomCode]);
