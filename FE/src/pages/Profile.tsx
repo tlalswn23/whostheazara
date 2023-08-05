eac776417e45f56421251502f53b690b80960bd3
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-05 22:40:42 +0900

    Design: 최근 전적 스크롤 추가

--- FE/src/pages/Profile.tsx
+++ FE/src/pages/Profile.tsx
@@ -52,7 +52,7 @@ const Profile = () => {
         </div>
         <div className="relative flex items-center 3xl:ml-[120px] ml-[96px]">
           <ProfileSideMenu viewMain={viewMain} onSetViewMain={onSetViewMain} />
-          <div className="3xl:min-w-[1140px] min-w-[912px] 3xl:h-[700px] h-[560px] 3xl:mx-[140px] mx-[112px] border-solid border-white 3xl:border-[20px] border-[15px] 3xl:text-[56px] text-[44.8px] font-bold bg-black">
+          <div className="3xl:min-w-[1140px] min-w-[912px] 3xl:h-[700px] h-[560px] 3xl:mx-[140px] mx-[112px] border-solid border-white 3xl:border-[20px] border-[15px] 3xl:text-[56px] text-[44.8px] font-bold bg-black overflow-scroll">
             {viewMain == PROFILE_MAP.NONE && (
               <ProfileBasic email={myInfo.email} nickname={myInfo.nickname} onSetViewMain={onSetViewMain} />
             )}
