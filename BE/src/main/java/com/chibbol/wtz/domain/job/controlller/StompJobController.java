db5c17641494b977ac17ff56ea095a828ac9501a
tlalswn23 <tlalswn23@daum.net>
2023-08-06 00:14:17 +0900

    Fix: 이전 버전과 합치면서 생긴 코드 수정

--- BE/src/main/java/com/chibbol/wtz/domain/job/controlller/StompJobController.java
+++ BE/src/main/java/com/chibbol/wtz/domain/job/controlller/StompJobController.java
@@ -1,18 +1,9 @@
 package com.chibbol.wtz.domain.job.controlller;
 
-<<<<<<< HEAD
 import com.chibbol.wtz.domain.job.dto.TargetUserDTO;
 import com.chibbol.wtz.domain.job.entity.UserAbilityRecord;
 import com.chibbol.wtz.domain.job.repository.UserAbilityRecordRedisRepository;
-=======
-import com.chibbol.wtz.domain.job.dto.UserAbilityRecordDTO;
-import com.chibbol.wtz.domain.job.entity.RoomUserJob;
-import com.chibbol.wtz.domain.job.entity.UserAbilityRecord;
-import com.chibbol.wtz.domain.job.repository.UserAbilityRecordRedisRepository;
-import com.chibbol.wtz.domain.job.service.JobService;
-import com.chibbol.wtz.domain.job.service.RedisJobPublisher;
-import com.chibbol.wtz.domain.job.service.StompJobService;
->>>>>>> 83a5752e34a64840388b6df0b76eaee05f7d139e
+
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +11,6 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
-<<<<<<< HEAD
-=======
-import java.util.List;
->>>>>>> 83a5752e34a64840388b6df0b76eaee05f7d139e
 
 @Slf4j
 @Controller
@@ -31,19 +18,6 @@ import java.util.List;
 public class StompJobController {
     private final UserAbilityRecordRedisRepository userAbilityRecordRepository;
 
-<<<<<<< HEAD
-=======
-    @Operation(summary = "게임 시작할때 roomSeq방에 참여한 인원들에게 랜덤으로 직업 부여")
-    @MessageMapping("/job/randomJob/{roomSeq}")
-    public void randomJobInRoomUser(@DestinationVariable Long roomSeq) {
-        List<RoomUserJob> list = jobService.randomJobInRoomUser(roomSeq);
-        String topicRoomSeq = String.valueOf(roomSeq);
-        stompJobService.addJobTopic(topicRoomSeq);
-        redisJobPublisher.publish(stompJobService.getTopic(topicRoomSeq), list);
-    }
-
-    // 얘는 됨
->>>>>>> 83a5752e34a64840388b6df0b76eaee05f7d139e
     @Operation(summary = "능력 사용 정보")
     @MessageMapping("/{roomSeq}/ability")
     public void recordAbility(@DestinationVariable Long roomSeq, TargetUserDTO targetUserDTO) {
