8c3d716d444f679759246f2bbd9121f6e971e57a
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-06 17:40:33 +0900

    Feat: SHOP 구매 이후 상점 리로드 로직 추가, 장착 api 추가

--- FE/src/api/axios/useShopApiCall.tsx
+++ FE/src/api/axios/useShopApiCall.tsx
@@ -3,6 +3,7 @@ import shopUrl from "../url/shopUrl";
 import { useAxiosIntercept } from "./useAxiosIntercept";
 import { toast } from "react-toastify";
 import { ERROR_CODE_MAP } from "../../constants/error/ErrorCodeMap";
+import { ShopItemType } from "../../types/ShopType";
 
 export const useShopApiCall = () => {
   const interceptAxiosInstance = useAxiosIntercept();
@@ -82,9 +83,64 @@ export const useShopApiCall = () => {
       throw error;
     }
   };
+
+  const getEquippedItems = async () => {
+    const url = shopUrl.getEquipped();
+    try {
+      const { data } = await interceptAxiosInstance.get(url);
+      const equippedCap = data.find((item: ShopItemType) => item.itemSeq >= 101 && item.itemSeq <= 199);
+      const equippedFace = data.find((item: ShopItemType) => item.itemSeq >= 201 && item.itemSeq <= 299);
+      const equippedClothing = data.find((item: ShopItemType) => item.itemSeq >= 301 && item.itemSeq <= 399);
+      return {
+        equippedCap,
+        equippedFace,
+        equippedClothing,
+      };
+    } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      const { status } = axiosError.response!;
+
+      switch (status) {
+        case ERROR_CODE_MAP.NOT_FOUND:
+          toast.error("이미 탈퇴한 회원입니다.");
+          break;
+        default:
+          toast.error("알 수 없는 에러가 발생했습니다, 관리자에게 문의해주세요.");
+          break;
+      }
+      throw error;
+    }
+  };
+
+  const equipItems = async (equippedItemList: ShopItemType[]) => {
+    const url = shopUrl.equip();
+    const body = { equippedItemList };
+    try {
+      await interceptAxiosInstance.patch(url, body);
+    } catch (error: unknown) {
+      const axiosError = error as AxiosError;
+      const { status } = axiosError.response!;
+
+      switch (status) {
+        case ERROR_CODE_MAP.NOT_POSSESS_ITEM:
+          toast.error("보유하지 않은 아이템은 장착 할 수 없습니다.");
+          break;
+        case ERROR_CODE_MAP.NOT_FOUND:
+          toast.error("이미 탈퇴한 회원입니다.");
+          break;
+        default:
+          toast.error("알 수 없는 에러가 발생했습니다, 관리자에게 문의해주세요.");
+          break;
+      }
+      throw error;
+    }
+  };
+
   return {
     getShopAllItem,
     buyItems,
     getCoin,
+    getEquippedItems,
+    equipItems,
   };
 };
