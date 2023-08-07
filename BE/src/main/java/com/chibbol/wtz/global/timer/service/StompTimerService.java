5ec4db290b254c623d80bc831b6fbf45c9a76f63
tlalswn23 <tlalswn23@daum.net>
2023-08-07 15:29:34 +0900

    Feat: Timer stomp 로직 수정 및 DTO 추가

---
+++ BE/src/main/java/com/chibbol/wtz/global/timer/service/StompTimerService.java
@@ -0,0 +1,26 @@
+package com.chibbol.wtz.global.timer.service;
+
+import com.chibbol.wtz.global.stomp.dto.dataDTO;
+import com.chibbol.wtz.global.stomp.service.RedisPublisherAll;
+import com.chibbol.wtz.global.stomp.service.StompService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StompTimerService {
+    private final RedisPublisherAll publisher;
+    private final StompService stompService;
+
+    public void sendToClient(String type, Long roomSeq, Object data){
+        stompService.addTopic(roomSeq); // 공통
+        publisher.publish(stompService.getTopic(roomSeq),
+                dataDTO.builder()
+                        .type(type)
+                        .roomSeq(roomSeq)
+                        .data(data)
+                        .build());
+    }
+}
