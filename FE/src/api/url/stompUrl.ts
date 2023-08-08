b54b75e38bcaa8d5f6a6aa09a012b22b2bc918dc
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-08 15:44:21 +0900

    Feat: ghost chat sub 추가

--- FE/src/api/url/stompUrl.ts
+++ FE/src/api/url/stompUrl.ts
@@ -5,6 +5,8 @@ export default {
   pubRoomJobSetting: (roomCode: string) => `/pub/room/${roomCode}/jobSetting`,
   pubRoomStart: (roomCode: string) => `/pub/room/${roomCode}/start`,
   subGame: (gameCode: string) => `/sub/game/${gameCode}/all`,
+  subGameZara: (gameCode: string) => `/sub/game/${gameCode}/zara`,
+  subGameGhost: (gameCode: string) => `/sub/game/${gameCode}/ghost`,
   // publishEnterMessage: () => `/pub/chat/enter`,
 
   pubGameTimer: (gameCode: string) => `/pub/game/${gameCode}/timer`,
