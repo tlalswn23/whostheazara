4d36bb17d12f0492e86612d40100477bf010b5fd
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-08 17:25:32 +0900

    Feat: game 채팅 pub 기능 추가

--- FE/src/api/url/stompUrl.ts
+++ FE/src/api/url/stompUrl.ts
@@ -14,4 +14,6 @@ export default {
   pubGameChatAll: (gameCode: string) => `/pub/game/${gameCode}/all/chat`,
   pubGameChatZara: (gameCode: string) => `/pub/game/${gameCode}/zara/chat`,
   pubGameChatGhost: (gameCode: string) => `/pub/game/${gameCode}/ghost/chat`,
+  pubGameVote: (gameCode: string) => `/pub/game/${gameCode}/vote`,
+  pubGameEndTimer: (gameCode: string) => `/pub/game/${gameCode}/timer`,
 };
