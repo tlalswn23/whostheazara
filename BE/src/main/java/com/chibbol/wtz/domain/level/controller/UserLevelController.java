07c3cfdac9d23d965886c31472a7bc376eeed224
hd9775 <kkang13277@naver.com>
2023-08-09 22:04:30 +0900

    Fix: build err

--- BE/src/main/java/com/chibbol/wtz/domain/level/controller/UserLevelController.java
+++ BE/src/main/java/com/chibbol/wtz/domain/level/controller/UserLevelController.java
@@ -1,16 +1,11 @@
 package com.chibbol.wtz.domain.level.controller;
 
-import com.chibbol.wtz.domain.job.entity.UserAbilityLog;
 import com.chibbol.wtz.domain.job.repository.UserAbilityLogRepository;
-import com.chibbol.wtz.domain.level.dto.LevelResultDTO;
 import com.chibbol.wtz.domain.level.service.UserLevelService;
-import com.chibbol.wtz.domain.point.dto.PointResultDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -20,15 +15,15 @@ public class UserLevelController {
     private final UserLevelService userLevelService;
     private final UserAbilityLogRepository userAbilityLogRepository;
 
-    @PatchMapping("/{gameCode}")
-    public ResponseEntity<Void> updateUserLevel(@PathVariable String gameCode){
-        List<UserAbilityLog> userAbilityLogs = userAbilityLogRepository.findAllByGameCode(gameCode);
-        List<LevelResultDTO> list = userLevelService.updateExp(userAbilityLogs);
-
-        for(LevelResultDTO temp : list){
-            log.info(temp.getUserSeq()+" "+temp.getLevel()+" "+temp.getCurrentExp()+" "+temp.getMaxExp()+" "+temp.getExpValue());
-        }
-
-        return ResponseEntity.ok().build();
-    }
+//    @PatchMapping("/{gameCode}")
+//    public ResponseEntity<Void> updateUserLevel(@PathVariable String gameCode){
+//        List<UserAbilityLog> userAbilityLogs = userAbilityLogRepository.findAllByGameCode(gameCode);
+//        List<LevelResultDTO> list = userLevelService.updateExp(userAbilityLogs);
+//
+//        for(LevelResultDTO temp : list){
+//            log.info(temp.getUserSeq()+" "+temp.getLevel()+" "+temp.getCurrentExp()+" "+temp.getMaxExp()+" "+temp.getExpValue());
+//        }
+//
+//        return ResponseEntity.ok().build();
+//    }
 }
