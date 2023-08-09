108c1e9835979185759f4b7bdf8589cc709bc265
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-09 15:15:22 +0900

    Fix: buil err

--- FE/src/components/room/RoomUserListItem.tsx
+++ FE/src/components/room/RoomUserListItem.tsx
@@ -12,14 +12,14 @@ export const RoomUserListItem = ({ nickname, order, isOwner }: RoomUserListItemP
   //TODO: 이미지 추가
 
   return (
-     <>
+    <>
       <div className="3xl:text-[30px] text-[24px] w-full 3xl:h-[50px] h-[40px] flex justify-center items-center flex-wrap">
         <p className="text-center">{nickname}</p>
       </div>
       <div className="relative">
         <img
           className="absolute 3xl:top-[-10px] top-[-8px] 3xl:left-[5px] left-[4px] 3xl:w-[250px] w-[200px] 3xl:h-[250px] h-[200px]"
-          src={RABBIT_MAP[key - 1].IMG[RABBIT_STATE_MAP.STAND]}
+          src={RABBIT_MAP[order].IMG[RABBIT_STATE_MAP.STAND]}
         />
         {isOwner && <img src="" alt="방장 표시" />}
       </div>
