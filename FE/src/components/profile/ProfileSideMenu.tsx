f05267affdded0c620feda175e4d49c88d962233
KYC <ghn05188@naver.com>
2023-08-11 13:51:14 +0900

    Feat: Profile 페이지 BGM 및 효과음 추가

--- FE/src/components/profile/ProfileSideMenu.tsx
+++ FE/src/components/profile/ProfileSideMenu.tsx
@@ -1,6 +1,7 @@
 import zaraImg from "../../assets/img/profile/zaraImg.png";
 import btnImg from "../../assets/img/common/blackBtnImg.png";
 import { PROFILE_MAP } from "../../constants/profile/ProfileMap";
+import { SFX, playSFX } from "../../utils/audioManager";
 
 interface profileSideMenuProps {
   onSetViewMain: (num: number) => void;
@@ -13,12 +14,12 @@ const ProfileSideMenu = ({ viewMain, onSetViewMain }: profileSideMenuProps) => {
       <img
         src={zaraImg}
         className="absolute 3xl:left-[-74px] left-[-59.2px] 3xl:top-[-120px] top-[-96px] 3xl:w-[200px] w-[160px] cursor-pointer"
-        onClick={() => onSetViewMain(PROFILE_MAP.PROFILE_BASIC)}
+        onClick={() => {onSetViewMain(PROFILE_MAP.PROFILE_BASIC); playSFX(SFX.TAB);}}
       />
       <div
         className={`3xl:w-[400px] w-[320px] 3xl:h-[200px] h-[160px] bg-contain bg-no-repeat bg-center flex items-center justify-center cursor-pointer`}
         style={{ backgroundImage: `url("${btnImg}")` }}
-        onClick={() => onSetViewMain(PROFILE_MAP.PROFILE_BASIC)}
+        onClick={() => {onSetViewMain(PROFILE_MAP.PROFILE_BASIC); playSFX(SFX.TAB);}}
       >
         <p
           className={`text-white 3xl:text-[45px] text-[36px] hover:text-amber-300 duration-500 w-full ${
@@ -35,7 +36,7 @@ const ProfileSideMenu = ({ viewMain, onSetViewMain }: profileSideMenuProps) => {
       <div
         className={`3xl:w-[400px] w-[320px] 3xl:h-[200px] h-[160px] bg-contain bg-no-repeat bg-center flex items-center justify-center cursor-pointer`}
         style={{ backgroundImage: `url("${btnImg}")` }}
-        onClick={() => onSetViewMain(PROFILE_MAP.PROFILE_RECENTLY_DATA)}
+        onClick={() => {onSetViewMain(PROFILE_MAP.PROFILE_RECENTLY_DATA); playSFX(SFX.TAB);}}
       >
         <p
           className={`text-white 3xl:text-[45px] text-[36px] w-full hover:text-amber-300 duration-500 ${
@@ -48,7 +49,7 @@ const ProfileSideMenu = ({ viewMain, onSetViewMain }: profileSideMenuProps) => {
       <div
         className={`3xl:w-[400px] w-[320px] 3xl:h-[200px] h-[160px] bg-contain bg-no-repeat bg-center flex items-center justify-center cursor-pointer`}
         style={{ backgroundImage: `url("${btnImg}")` }}
-        onClick={() => onSetViewMain(PROFILE_MAP.PROFILE_DATA)}
+        onClick={() => {onSetViewMain(PROFILE_MAP.PROFILE_DATA); playSFX(SFX.TAB);}}
       >
         <p
           className={`text-white 3xl:text-[45px] text-[36px] w-full hover:text-amber-300 duration-500 ${
