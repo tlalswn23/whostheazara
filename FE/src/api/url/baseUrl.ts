4e1f56f2c84b19709f417b25c11571a52110fa46
J3SUNG <wptjd6141@naver.com>
2023-08-11 14:21:34 +0900

    Feat: 밤 로직 진행 중

--- FE/src/api/url/baseUrl.ts
+++ FE/src/api/url/baseUrl.ts
@@ -7,12 +7,12 @@
 // export const stompBaseUrl = "ws://14.46.141.193:8080/api/v1/stomp";
 
 // 현덕 싸피노트북
-// export const baseUrl = "http://192.168.100.181:8080/api/v1";
-// export const stompBaseUrl = "ws://192.168.100.181:8080/api/v1/stomp";
+export const baseUrl = "http://192.168.100.181:8080/api/v1";
+export const stompBaseUrl = "ws://192.168.100.181:8080/api/v1/stomp";
 
 // 배포
-export const baseUrl = "https://i9d206.p.ssafy.io/api/v1";
-export const stompBaseUrl = "wss://i9d206.p.ssafy.io/api/v1/stomp";
+// export const baseUrl = "https://i9d206.p.ssafy.io/api/v1";
+// export const stompBaseUrl = "wss://i9d206.p.ssafy.io/api/v1/stomp";
 
 // 스타벅스
 // export const baseUrl = "http://172.29.88.216:8080/api/v1";
--- FE/src/components/game/GameVoteItem.tsx
+++ FE/src/components/game/GameVoteItem.tsx
@@ -11,13 +11,14 @@ export const GameVoteUser = ({ voteNum, userOrder, onSetSelectVote, isDie }: Gam
   return (
     <>
       <div
-        className={`3xl:w-[375px] w-[300px] 3xl:h-[250px] h-[200px] flex justify-end items-end 3xl:pr-[30px] pr-[24px] 3xl:pb-[20px] pb-[16px] ${
+        className={`3xl:w-[375px] w-[300px] 3xl:h-[250px] h-[200px] 3xl:pr-[30px] pr-[24px] 3xl:pb-[20px] pb-[16px] ${
           isDie === 0 && "cursor-pointer hover:brightness-75"
         }`}
-        onClick={() => onSetSelectVote(userOrder)}
       >
         {isDie === 0 && (
-          <p className="text-red-600 3xl:text-[60px] text-[48px] font-bold drop-shadow-stroke-white">{voteNum}</p>
+          <div className="w-full h-full flex justify-end items-end" onClick={() => onSetSelectVote(userOrder)}>
+            <p className="text-red-600 3xl:text-[60px] text-[48px] font-bold drop-shadow-stroke-white">{voteNum}</p>
+          </div>
         )}
       </div>
     </>
