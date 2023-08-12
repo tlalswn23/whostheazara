fffc67739dc8cebd9939956663d19bafb36278cc
J3SUNG <wptjd6141@naver.com>
2023-08-12 18:34:18 +0900

    Feat: DayAlert 수정

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -20,15 +20,6 @@ const Home = () => {
 
   return (
     <HomeLayout>
-      <Link to="game/1">
-        <p className="absolute top-[0px] left-[400px] w-[200px] h-[200px] text-red-200 text-[50px]">예찬(1)</p>
-      </Link>
-      <Link to="game/2">
-        <p className="absolute top-[0px] left-[600px] w-[200px] h-[200px] text-green-200 text-[50px]">제성(2)</p>
-      </Link>
-      <Link to="game/3">
-        <p className="absolute top-[0px] left-[800px] w-[200px] h-[200px] text-sky-200 text-[50px]">슬호(3)</p>
-      </Link>
       <HomeSideMenu showModalHandler={showModalHandler} />
       <LoginFormModal curModalType={curModalType} showModalHandler={showModalHandler} />
       <SignupFormModal curModalType={curModalType} showModalHandler={showModalHandler} />
