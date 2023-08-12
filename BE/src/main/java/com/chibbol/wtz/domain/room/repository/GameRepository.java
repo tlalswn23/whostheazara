7bd0a85c7364e2471b990823461268313c7728ef
chococuki <kkang13277@naver.com>
2023-08-12 18:38:44 +0900

    Test: 테스트 코드 추가

--- BE/src/main/java/com/chibbol/wtz/domain/room/repository/GameRepository.java
+++ BE/src/main/java/com/chibbol/wtz/domain/room/repository/GameRepository.java
@@ -1,10 +1,11 @@
 package com.chibbol.wtz.domain.room.repository;
 
 import com.chibbol.wtz.domain.room.entity.Game;
+import com.chibbol.wtz.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+    Room findRoomByGameCode(String gameCode);
 }
