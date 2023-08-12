2f23fdd3b08f6d1d12872927e1575ed2fc0ee273
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-12 23:20:13 +0900

    Fix: socket unbeforeunload 삭제

--- FE/src/context/socketContext.tsx
+++ FE/src/context/socketContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { LayoutChildrenProps } from "../types/LayoutChildrenProps";
-import { Client, StompHeaders } from "@stomp/stompjs";
+import { Client } from "@stomp/stompjs";
 import { useAccessTokenState } from "./accessTokenContext";
 import { stompBaseUrl } from "../api/url/baseUrl";
 
@@ -38,29 +38,8 @@ export const WebSocketProvider = ({ children }: LayoutChildrenProps) => {
       },
     });
 
-    const handleBeforeUnload = () => {
-      if (newClient.connected) {
-        // 필요한 헤더와 함께 메시지를 보낼 수 있습니다.
-        const headers: StompHeaders = {
-          customHeader: "value", // 원하는 헤더를 여기에 추가하세요
-        };
-        newClient.publish({ destination: "/your/destination", headers: headers, body: "your-message" });
-
-        // 웹 소켓 연결을 종료하는 코드도 추가할 수 있습니다.
-        // newClient.deactivate();
-      }
-    };
-
-    // beforeunload 이벤트를 등록합니다.
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
     setClient(newClient);
     newClient.activate();
-
-    return () => {
-      // 컴포넌트가 언마운트되면 이벤트를 제거합니다.
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
   }, [accessToken]);
 
   return <WebSocketContext.Provider value={{ client }}>{children}</WebSocketContext.Provider>;
