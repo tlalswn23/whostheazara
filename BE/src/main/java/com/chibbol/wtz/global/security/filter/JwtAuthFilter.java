21f7bc5a97860f87900134bd83dde385a69675a2
tlalswn23 <tlalswn23@daum.net>
2023-08-12 12:51:48 +0900

    Fix: openvidu 권한 설정 추가

--- BE/src/main/java/com/chibbol/wtz/global/security/filter/JwtAuthFilter.java
+++ BE/src/main/java/com/chibbol/wtz/global/security/filter/JwtAuthFilter.java
@@ -117,8 +117,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         matchers.add(new AntPathRequestMatcher("/actuator/**"));
         matchers.add(new AntPathRequestMatcher("/stomp/**"));
 
-        matchers.add(new AntPathRequestMatcher("/api/sessions/**"));
-        matchers.add(new AntPathRequestMatcher("/api/sessions"));
+        matchers.add(new AntPathRequestMatcher("/api/v1/sessions/**"));
+        matchers.add(new AntPathRequestMatcher("/api/v1/sessions"));
 
         // 요청 URL이 permitAll()로 허용한 URL 패턴에 해당하는지 확인
         for (RequestMatcher matcher : matchers) {
