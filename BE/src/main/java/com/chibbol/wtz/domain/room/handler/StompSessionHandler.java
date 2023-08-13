3502b70b9f6e4e89f5d725c1cf4621c9be8d19a8
YeahLim <yeahleem2@gmail.com>
2023-08-13 11:24:22 +0900

    Fix: ExceptionHandler 추가

--- BE/src/main/java/com/chibbol/wtz/domain/room/handler/StompSessionHandler.java
+++ BE/src/main/java/com/chibbol/wtz/domain/room/handler/StompSessionHandler.java
@@ -1,2 +1,28 @@
-package com.chibbol.wtz.domain.room.handler;public class StompSessionHandler {
+package com.chibbol.wtz.domain.room.handler;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+
+@Slf4j
+public class StompSessionHandler extends StompSessionHandlerAdapter {
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        // 메시지 프레임 처리
+    }
+
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+        // 예외 처리
+        log.info("에러 발생: " + exception.getMessage());
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception) {
+        log.info("전송 에러 메세지: " + exception.getMessage());
+    }
 }
