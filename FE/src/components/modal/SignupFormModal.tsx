a7865f7f1241e87571e486e047e542600e9f9507
J3SUNG <wptjd6141@naver.com>
2023-08-13 18:38:13 +0900

    Fix: UI 수정

--- FE/src/components/modal/SignupFormModal.tsx
+++ FE/src/components/modal/SignupFormModal.tsx
@@ -94,7 +94,7 @@ const SignupFormModal = ({ curModalType, showModalHandler }: FormModalProps) => 
           <div className="3xl:mb-[8px] mb-[6.4px] w-[69%]">
             <label className="3xl:ml-[4px] ml-[3.2px] 3xl:text-[24px] text-[19.2px]">이메일</label>
             <input
-              className=" cursor-yellow 3xl:h-[40px] h-[32px] border-solid border-black 3xl:border-[2px] border-[1.6px] w-full 3xl:text-[24px] text-[19.2px]"
+              className=" cursor-yellow 3xl:h-[40px] h-[32px] border-solid border-black 3xl:border-[2px] border-[1.6px] w-full 3xl:text-[24px] text-[19.2px] 3xl:px-[10px] px-[8px]"
               onChange={(e) => emailField.onChange(e.target.value)}
               value={emailField.value}
             />
@@ -116,7 +116,7 @@ const SignupFormModal = ({ curModalType, showModalHandler }: FormModalProps) => 
         <div className="3xl:mb-[8px] mb-[6.4px] w-full">
           <label className="3xl:ml-[4px] ml-[3.2px] 3xl:text-[24px] text-[19.2px]">인증코드</label>
           <input
-            className=" cursor-yellow 3xl:h-[40px] h-[32px] border-solid border-black 3xl:border-[2px] border-[1.6px] w-full 3xl:text-[24px] text-[19.2px]"
+            className=" cursor-yellow 3xl:h-[40px] h-[32px] border-solid border-black 3xl:border-[2px] border-[1.6px] w-full 3xl:text-[24px] text-[19.2px] 3xl:px-[10px] px-[8px]"
             onChange={(e) => setVerificationCode(e.target.value)}
             value={verificationCode}
           />
