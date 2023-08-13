698ba3373d7d995e03289c57aa649a4dd00fa9cb
KYC <ghn05188@naver.com>
2023-08-13 13:22:51 +0900

    Feat: 게임 로직에 맞춘 카메라 오디오 설정

--- FE/src/components/game/GameMenu.tsx
+++ FE/src/components/game/GameMenu.tsx
@@ -39,27 +39,28 @@ export const GameMenu = ({
   const [micOn, setMicOn] = useState(true);
   const [cameraOn, setCameraOn] = useState(true);
 
-  const canIChangeSetting = (): boolean => {
+  const canIChangeSetting = (): boolean => {    
     switch (nowTime) {
-      case "DAY":
+      case "DAY": // 낮일때, 죽은 사람 제외 모두 카메라 켜기 가능
       case "VOTE":
         if (amIDead) {
           return false;
         }
         return true;
-      case "VOTE_RESULT":
+      case "VOTE_RESULT": // 투표 결과 나올 때 투표당한 사람만 카메라 켜기 가능
         if (amIVoted) {
           return true;
         }
         return false;
-      case "NIGHT":
-      case "NIGHT_RESULT":
+      case "NIGHT": // 밤에 자라만 카메라 켜기 가능
         if (amIDead) {
           return false;
         } 
         if (amIZara) {
           return true;
         } 
+        return false;
+      case "NIGHT_RESULT":  // 밤 결과 나올 때 모두 카메라 끄기        
         return false;
       default:
         console.error("unknown data - nowTime: ", nowTime);
@@ -69,7 +70,7 @@ export const GameMenu = ({
   }
 
   const onClickSoundOn = () => {    
-    if (!canIChangeSetting) {
+    if (!canIChangeSetting()) {
       setGameAudio(!soundOn);
       setSoundOn(!soundOn);
       return;
@@ -80,7 +81,7 @@ export const GameMenu = ({
   }
 
   const onClickCameraOn = () => {
-    if (!canIChangeSetting) {
+    if (!canIChangeSetting()) {
       return;
     }
     setMyMic(!micOn);
@@ -88,7 +89,7 @@ export const GameMenu = ({
   }
 
   const onClickMicOn = () => {
-    if (!canIChangeSetting) {
+    if (!canIChangeSetting()) {
       return;
     }
     setMyCamera(!cameraOn);
@@ -103,47 +104,58 @@ export const GameMenu = ({
   }
 
   useEffect(() => {    
+    console.log("NOWTIMETEST nowtime: ", nowTime);
+    console.log("NOWTIMETEST canIChange: ", canIChangeSetting());
+    console.log("NOWTIMETEST amIZara: ", amIZara);
+    console.log("NOWTIMETEST amIDead: ", amIDead);
+    console.log("NOWTIMETEST amIVoted: ", amIVoted);
     let bgm: HTMLAudioElement;
     switch (nowTime) {
-      case "DAY":
+      case "DAY": // 낮
       case "VOTE":
         bgm = createBGMInstance(BGM.DAY);
-        setAmIVoted(false);
-        setUserVideo(true);
-        setUserAudio(soundOn);
+        setAmIVoted(false); // 투표 결과 리셋
+        setUserVideo(true); // 다른 유저 영상 보이게 하기
+        setUserAudio(soundOn);  // 유저 사운드는 현재 사운드 설정에 맞추기
         if (amIDead) {
-          setMyCameraMicOn(false);
+          setMyCameraMicOn(false);  // 죽었으면 카메라 마이크 끄기
           break;
         }
-        setMyCameraMicOn(true);    
+        setMyCameraMicOn(true); // 살았으면 카메라 마이크 켜기
         break;
-      case "VOTE_RESULT":
+      case "VOTE_RESULT": // 투표 결과 나올 때
         bgm = createBGMInstance(BGM.RESULT);
-        setUserVideo(true);
-        setUserAudio(soundOn);
+        setUserVideo(true); // 다른 유저 영상 보이게 하기
+        setUserAudio(soundOn);  // 유저 사운드는 현재 사운드 설정에 맞추기
         if (amIVoted) {
-          setMyCameraMicOn(true);
+          setMyCameraMicOn(true); // 투표 당한 사람만 카메라 마이크 켜기
           break;
         }
-        setMyCameraMicOn(false);    
+        setMyCameraMicOn(false);  // 투표 안당한 사람은 카메라 마이크 끄기
         break;
-      case "NIGHT":
-      case "NIGHT_RESULT":
+      case "NIGHT": // 밤
         bgm = createBGMInstance(BGM.NIGHT);
-        if (amIDead) {
-          setUserVideo(true);
-          setUserAudio(soundOn);
-          setMyCameraMicOn(false);
+        if (amIDead) {  // 죽었으면
+          setUserVideo(true); // 다른 사람 영상 볼 수 있음
+          setUserAudio(soundOn);  // 소리도 들을 수 있음
+          setMyCameraMicOn(false);  // 내 카메라 마이크 끄기
           break;
         } 
-        if (amIZara) {
-          setUserVideo(true);
-          setUserAudio(soundOn);
-          setMyCameraMicOn(true);
+        if (amIZara) {  // 내가 자라이면
+          setUserVideo(true); // 다른 사람 영상 볼 수 있음
+          setUserAudio(soundOn);  // 소리도 들을 수 있음
+          setMyCameraMicOn(true); // 내 카메라 마이크 켜기
           break;
         }
-        setUserVideo(false);
+        // 자라가 아니면 내 카메라 마이크 끄고 다른사람 영상 소리 못 보고 들음
+        setUserVideo(false);  
         setUserAudio(false);    
+        setMyCameraMicOn(false);
+        break;        
+      case "NIGHT_RESULT":
+        // 밤 결과 나올 때 모든 유저 카메라 마이크 끄고 영상 소리 못 보고 들음
+        setUserVideo(false);
+        setUserAudio(false);
         setMyCameraMicOn(false);
         break;
       default:
