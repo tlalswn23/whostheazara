60948ba9beabc4ffb968fa1969ba5a86ab8fce78
hd9775 <kkang13277@naver.com>
2023-08-14 14:46:43 +0900

    Fix: 마피아 2명일 경우 경찰 능력 수정

--- BE/src/main/java/com/chibbol/wtz/domain/job/type/Mafia.java
+++ BE/src/main/java/com/chibbol/wtz/domain/job/type/Mafia.java
@@ -24,8 +24,12 @@ public class Mafia implements JobInterface {
         if(turnResult.containsKey("mafia_use")) {
             // 이전 마피아 선택보다 현재 마피아 선택이 늦었을때
             if(turnResult.get("mafia_use") < useTime) {
+                if(turnResult.containsKey("mafia")) {
+                    turnResult.put("mafia2", userSeq);
+                } else {
+                    turnResult.put("mafia", userSeq);
+                }
                 turnResult.put("kill", targetUserSeq);
-                turnResult.put("mafia", userSeq);
                 turnResult.put("mafia_use", useTime);
             }
         } else {
