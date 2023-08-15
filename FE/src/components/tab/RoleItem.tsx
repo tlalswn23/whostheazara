bf6a1a858e33075f4b35092f6f1347ac30913cdf
J3SUNG <wptjd6141@naver.com>
2023-08-15 13:16:19 +0900

    Feat: 게임설명 수정

--- FE/src/components/tab/RoleItem.tsx
+++ FE/src/components/tab/RoleItem.tsx
@@ -6,7 +6,7 @@ interface RoleItemProps {
 }
 const RoleItem = ({ name, desc, imgPath, color }: RoleItemProps) => {
   return (
-    <div className="flex items-center 3xl:mb-[32px] mb-[25.6px]">
+    <div className="flex items-center 3xl:mb-[32px] mb-[25.6px] 3xl:h-[84px] h-[67.2px]">
       <img src={imgPath} className="3xl:w-[80px] w-[64px] 3xl:h-[80px] h-[64px]" />
       <p
         className={`3xl:mx-[40px] mx-[32px] 3xl:w-[120px] w-[96px] text-center 3xl:text-[28px] text-[22.4px] font-bold ${color}`}
--- FE/src/components/tab/StoryContent.tsx
+++ FE/src/components/tab/StoryContent.tsx
@@ -3,29 +3,20 @@ import StoryImg from "../../assets/img/home/StoryImg.png";
 const StoryContent = () => {
   return (
     <div className="text-center 3xl:text-[28px] text-[22.4px] 3xl:leading-[60px] leading-[48px]">
-      <p>아픈 용왕을 위해 토끼의 간을 찾으러 나선 자라...</p>
-      <p>토끼의 시력이 나쁜점을 이용해 자라는 토끼로 변장했다!</p>
-      <p>하지만 토끼는 자라들이 숨어들어 왔다는 것을 알게 되었고...</p>
-      <p>
-        그렇게 <TextRabbit />와 <TextZara />의 숨막히는 <span className="text-red-300 font-bold">혈전</span>이 시작된다!
-      </p>
+      <p>1. 어느 날, 바다의 용왕은 이유모를 큰 병에 걸린다.</p>
+      <p>용왕에게는 소문난 충신 자라가 있었는데,</p>
+      <p>자라는 용왕을 위해 명약이라 소문난 토끼의 간을 찾아나선다.</p>
+      <img className="m-auto 3xl:h-[220px] h-[176px]" src={StoryImg} />
+
+      <p>2. 한 달에 한 번 토끼들이 배를 타고 이동하는 날,</p>
+      <p>자라는 토끼들이 시력이 나쁜 점을 이용하여 변장 후 잠입한다!</p>
+      <img className="m-auto 3xl:h-[220px] h-[176px]" src={StoryImg} />
+
+      <p>3. 자라가 배에 숨어들었다는 소식을 듣게 된 토끼들,</p>
+      <p>그들은 과연 숨은 자라를 찾아내고 무사히 배에서 내릴 수 있을까?</p>
       <img className="m-auto 3xl:h-[220px] h-[176px]" src={StoryImg} />
     </div>
   );
 };
 
-const TextZara = () => {
-  return (
-    <>
-      <span className="text-green-200 font-bold">자라</span>
-    </>
-  );
-};
-const TextRabbit = () => {
-  return (
-    <>
-      <span className="text-yellow-200 font-bold">토끼</span>
-    </>
-  );
-};
 export default StoryContent;
