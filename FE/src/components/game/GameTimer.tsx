6465d9a395c03c28d496869fc968baf10f1d0f9d
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-16 03:07:38 +0900

    Feat: Target 수정, 스킵버튼 수정

--- FE/src/components/game/GameTimer.tsx
+++ FE/src/components/game/GameTimer.tsx
@@ -54,20 +54,16 @@ export const GameTimer = ({ timer, setTimer, nowTime }: GameTimerProps) => {
 
   return (
     <div className="absolute 3xl:top-[20px] top-[16px] drop-shadow-2xl w-[20%] flex items-center justify-center">
-      <div className="3xl:w-[80px] w-[64px] 3xl:ml-[40px] ml-[32px]" />
-      <p className="text-white 3xl:text-[120px] text-[96px] drop-shadow-stroke-black text-center 3xl:w-[150px] w-[120px]">
-        {timer}
-      </p>
+      <p className="text-white 3xl:text-[120px] text-[96px] drop-shadow-stroke-black text-center">{timer}</p>
       <div
-        className={`text-black text-center font-bold 3xl:w-[80px] w-[64px] 3xl:h-[80px] h-[64px] flex justify-center items-center 3xl:ml-[20px] ml-[16px] 3xl:mt-[10px] mt-[8px] 3xl:text-[32px] text-[25.6px] 3xl:border-[5px] border-[4px] border-black flex-wrap hover:brightness-110 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-gray-200 via-gray-400 to-gray-600 ${
+        className={`absolute top-[160px] text-yellow-200 text-center font-bold 3xl:w-[80px] w-[64px] 3xl:h-[80px] h-[64px] flex justify-center items-center hover:brightness-110  ${
           (useSkip || nowTime !== "DAY") && "opacity-0"
         }`}
         onClick={() => skipTime()}
       >
-        <p className="3xl:text-[18px] text-[14.4px] 3xl:w-[80px] w-[64px] 3xl:h-[80px] h-[64px] 3xl:mt-[10px] mt-[8px] 3xl:mb-[-70px] mb-[-56px]">
-          {"-10"}
+        <p className="3xl:text-[50px] text-[40px] 3xl:w-[150px] w-[120px] 3xl:h-[80px] h-[64px] drop-shadow-stroke-black">
+          SKIP
         </p>
-        <p className="3xl:text-[40px] text-[32px] w-full h-full 3xl:pl-[10px] pl-[8px]">{">>"}</p>
       </div>
     </div>
   );
