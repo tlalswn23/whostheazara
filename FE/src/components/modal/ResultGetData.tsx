4973d556293e3445a277590e1b8123a386efc47a
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-17 01:02:24 +0900

    Feat: 코인 흭득 추가

--- FE/src/components/modal/ResultGetData.tsx
+++ FE/src/components/modal/ResultGetData.tsx
@@ -1,5 +1,7 @@
 import Rodal from "rodal";
 import { useState, useEffect } from "react";
+import rabbit from "../../assets/img/home/RabbitOnly.png";
+import coin from "../../assets/img/shop/coin.png";
 
 interface ResultGetDataProps {
   levelUp: boolean;
@@ -29,11 +31,27 @@ const ResultGetData = ({
   const [levelEffect, setLevelEffect] = useState(0);
   const levelColor = ["text-yellow-200", "text-blue-200", "text-red-200"];
   const levelScale = ["scale-100", "scale-110"];
+  const [point, setPoint] = useState(lastPoint);
+  const [addPoint, setAddPoint] = useState(0);
+  const [dropCoinView, setDropCoinView] = useState(false);
 
   useEffect(() => {
     setLevel(lastLevel);
     setGauge((lastExp / maxExp) * 100);
-  }, [lastLevel, lastExp, maxExp]);
+    setAddPoint(currentPoint - lastPoint);
+  }, [lastLevel, lastExp, maxExp, lastPoint, currentPoint]);
+
+  useEffect(() => {
+    if (addPoint <= 0) {
+      setDropCoinView(false);
+      return;
+    }
+    setTimeout(() => {
+      setAddPoint((prev) => prev - 1);
+      setPoint((prev) => prev + 1);
+      setDropCoinView(true);
+    }, 100);
+  }, [addPoint]);
 
   useEffect(() => {
     if (levelUp && level === currentLevel) {
@@ -70,7 +88,7 @@ const ResultGetData = ({
       setTimeout(() => {
         setGaugeFull(false);
         setGauge((currentExp / maxExp) * 100);
-      }, 100);
+      }, 200);
     }
   }, [gaugeFull]);
   return (
@@ -88,7 +106,20 @@ const ResultGetData = ({
         backgroundColor: "rgba(0, 0, 0, 0)",
       }}
     >
-      <div className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 3xl:w-[800px] w-[640px] 3xl:h-[480px] h-[384px] bg-gray-900 border-solid 3xl:border-[10px] border-[8px] border-white flex flex-col justify-center items-center 3xl:text-[30px] text-white text-[24px]">
+      <div className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 3xl:w-[800px] w-[640px] 3xl:h-[400px] h-[384px] bg-gray-900 border-solid 3xl:border-[10px] border-[8px] border-white flex flex-col justify-center items-center 3xl:text-[30px] text-white text-[24px] pt-[30px]">
+        <div className="flex justify-between items-center w-full mb-[80px]">
+          <div className="flex justify-start items-center w-auto ml-[60px]">
+            <img className="w-[60px] h-[60px] mx-[20px]" src={rabbit} />
+            <p className="text-[40px] font-bold text-yellow-200">Who's the ZARA</p>
+          </div>
+          <div className="flex justify-end items-center mr-[70px]">
+            {dropCoinView && (
+              <img className={`absolute right-[140px] w-[80px] h-[80px] animate-drop-fade-out`} src={coin} />
+            )}
+            <img className="absolute right-[140px] w-[80px] h-[80px]" src={coin} />
+            <p className="absolute right-[60px] w-[80px] font-bold text-left">{point}</p>
+          </div>
+        </div>
         <div className="relative w-[80%] 3xl:ml-[30px] ml-[24px] 3xl:h-[80px] h-[64px] 3xl:border-[8px] border-[6.4px] rounded-3xl border-gray-700 3xl:my-[40px] my-[32px] bg-gradient-to-br from-rose-100 to-teal-100">
           <div
             className={`absolute top-0 h-[66px] rounded-xl 3xl:border-r-[8px] border-r-[6.4px] border-gray-600 bg-gradient-to-br from-yellow-200 via-yellow-300 to-yellow-400 scale-x-100 ease-linear ${
