5af42226748c4cec15a4f826b86565bcf8f479aa
Jeong seulho <wjdtmfgh@gmail.com>
2023-08-16 10:10:09 +0900

    Fix: build err

--- FE/src/components/game/GameNight.tsx
+++ FE/src/components/game/GameNight.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { GameNightTarget } from "./GameNightTarget";
 import { useWebSocket } from "../../context/socketContext";
 import { useParams } from "react-router-dom";
