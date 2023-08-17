4a71bea9d7b23021d52282a03c048d7cc9de4c43
J3SUNG <wptjd6141@naver.com>
2023-08-17 16:02:41 +0900

    Feat: 밤 타겟 위치 변경

--- FE/src/components/game/GameNightTarget.tsx
+++ FE/src/components/game/GameNightTarget.tsx
@@ -71,7 +71,7 @@ export const GameNightTarget = ({
           </div>
         )}
         {amIDead && (
-          <div className="absolute bottom-[6%] right-[4%] 3xl:w-[125px] w-[100px] 3xl:h-[125px] h-[100px] flex flex-wrap-reverse justify-end items-start">
+          <div className="absolute bottom-[5%] right-[4%] 3xl:w-[125px] w-[100px] 3xl:h-[125px] h-[100px] flex flex-wrap-reverse justify-end items-start">
             {orderNo === targetList[0] && <img className="w-[40%]" src={JOB_MAP[2].imgColor} />}
             {orderNo === targetList[1] && <img className="w-[40%]" src={JOB_MAP[3].imgColor} />}
             {orderNo === targetList[2] && <img className="w-[40%]" src={JOB_MAP[4].imgColor} />}
