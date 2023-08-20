3622e3637a114104fa98dc99ae8f4747c93be854
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-21 00:57:35 +0900

    Feat: 마이크 화면 허용 알림

--- FE/src/components/modal/CamMicPermissionModal.tsx
+++ FE/src/components/modal/CamMicPermissionModal.tsx
@@ -1,7 +1,6 @@
 import Rodal from "rodal";
 import { FormModalProps } from "../../types/FormModalProps";
 import { Modal_Category_Map } from "../../constants/home/ModalCategoryMap";
-import loginBox from "../../assets/img/home/loginBox.png";
 import camMicPermission from "../../assets/img/home/camMicPermission.gif";
 
 const CamMicPermissionModal = ({ curModalType, showModalHandler }: FormModalProps) => {
@@ -23,15 +22,9 @@ const CamMicPermissionModal = ({ curModalType, showModalHandler }: FormModalProp
         boxShadow: "none",
       }}
     >
-      <img
-        src={loginBox}
-        className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 3xl:min-w-[560px] min-w-[448px] 3xl:h-[480px] h-[384px] bg-transparent"
-      />
-      <img
-        src={camMicPermission}
-        className="absolute left-[50%] top-[100%] transform -translate-x-1/2 -translate-y-1/2 3xl:min-w-[460px] min-w-[408px] 3xl:h-[240px] min-h-[204px]"
-      />
-      CamMicPermissionModal
+      <div className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 3xl:w-[1800px] w-[1440px] bg-transparent border-solid border-white 3xl:border-[10px] border-[8px]">
+        <img src={camMicPermission} className="w-full" />
+      </div>
     </Rodal>
   );
 };
