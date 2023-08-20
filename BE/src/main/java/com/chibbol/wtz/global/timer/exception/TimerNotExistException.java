acaf7f8863c42dc552e1c22d9275e07df2dda44c
chococuki <kkang13277@naver.com>
2023-08-20 15:54:50 +0900

    Rename: 안쓰는 파일 삭제

--- BE/src/main/java/com/chibbol/wtz/global/timer/exception/TimerNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.chibbol.wtz.global.timer.exception;
-
-public class TimerNotExistException extends RuntimeException {
-    public TimerNotExistException(String message) {
-        super(message);
-    }
-}
