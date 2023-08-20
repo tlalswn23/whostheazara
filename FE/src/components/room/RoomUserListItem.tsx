00777d91285e332cab663107b9f03c744c7d8387
wptjd6141@naver.com <wptjd6141@naver.com>
2023-08-20 23:42:08 +0900

    Feat: 방장, 준비완료 UI 변경

--- FE/src/components/room/RoomUserListItem.tsx
+++ FE/src/components/room/RoomUserListItem.tsx
@@ -1,6 +1,5 @@
 import { RABBIT_MAP } from "../../constants/common/RabbitMap";
 import { RABBIT_STATE_MAP } from "../../constants/game/RabbitStateMap";
-import { OWNER_IMG_MAP } from "../../constants/room/ownerImgMap";
 import { useAccessTokenState } from "../../context/accessTokenContext";
 interface RoomUserListItemProps {
   nickname: string;
@@ -28,10 +27,9 @@ export const RoomUserListItem = ({
   return (
     <>
       <div className="3xl:text-[30px] text-[24px] w-full 3xl:h-[50px] h-[40px] flex justify-center items-center flex-wrap">
-        <p className="text-center text-white">
+        <p className="text-center text-white 3xl:mt-[15px] mt-[12px]">
           {nickname}
           {myUserSeq === userSeq && " (나)"}
-          {ready ? " (준비완료)" : ""}
         </p>
       </div>
       <div className="relative h-full">
@@ -59,8 +57,16 @@ export const RoomUserListItem = ({
         )}
       </div>
 
-      {ownerSeq === userSeq && (
-        <img src={OWNER_IMG_MAP[order]} alt="방장 표시" className=" absolute bottom-0 w-full h-1/5" />
+      {ownerSeq === userSeq ? (
+        <p className=" absolute 3xl:bottom-[10px] bottom-[8px] w-full text-green-500 text-center 3xl:text-[40px] text-[32px]">
+          방장
+        </p>
+      ) : (
+        ready && (
+          <p className=" absolute 3xl:bottom-[10px] bottom-[8px] w-full text-yellow-500 text-center 3xl:text-[40px] text-[32px]">
+            준비 완료
+          </p>
+        )
       )}
     </>
   );
